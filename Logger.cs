185dfd5c937da4995d0599a9ed402e68b590a1c0
DannyCKing <danny.c.king@gmail.com>
2019-01-31 10:10:29 -0700

    Update Logger.cs
    Adding exception message to output.  It was unused before.  If failure to log to desired location, output in local output folder instead.

--- Logger.cs
+++ Logger.cs
@@ -1,44 +1,68 @@
-public class Logger
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace MockServer.Utilities
 {
-    private const string LOG_FILE_NAME = "log.txt";
-
-    public static void Log(string message, Exception e = null)
+    public class Logger
     {
-        try
+        private const string LOG_FILE_NAME = "log.txt";
+
+        public static void Log(string message, Exception e = null)
         {
-            string folderPath = "APP_NAME";// put your app name here
+            string originalLogMessage = "";
+            try
+            {
+                string folderPath = "APP_NAME";// put your app name here
 
-            var fileName = Path.Combine(folderPath, LOG_FILE_NAME);
+                var pathToLogFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), folderPath);
 
-            File.AppendAllText(fileName, GetLogMessage(message, e));
-        }
-        catch (Exception logException)
-        {
-            // Unable to log to file
-            Console.WriteLine("Unable to log to file");
-            Console.WriteLine(GetLogMessage(message, e));
-        }
-    }
+                if(!Directory.Exists(pathToLogFile))
+                {
+                    Directory.CreateDirectory(pathToLogFile);
+                }
+                var fileName = Path.Combine(pathToLogFile, LOG_FILE_NAME);
 
-    private static string GetLogMessage(string message, Exception e)
-    {
-        string exceptionMessage = GetExceptionString(e);
-        string logMessage = DateTime.Now.ToLongDateString();
-        logMessage += " " + message + Environment.NewLine;
+                originalLogMessage = GetLogMessage(message, e);
+                File.AppendAllText(fileName, originalLogMessage);
+            }
+            catch (Exception logException)
+            {
+                // Unable to log to file
+                Console.WriteLine("Unable to log to file");
+                Console.WriteLine(GetLogMessage(message, e));
 
-        return logMessage;
-    }
-
-    private static string GetExceptionString(Exception error)
-    {
-        string errorString = "";
-        Exception currentError = error;
-        while (currentError != null)
-        {
-            errorString += currentError.Message + " ";
-            currentError = currentError.InnerException;
+                File.WriteAllText("local_output.txt", GetLogMessage("Unable to write to desired file", logException));
+                File.WriteAllText("local_output.txt", originalLogMessage);
+            }
         }
 
-        return errorString;
+        private static string GetLogMessage(string message, Exception e = null)
+        {
+            string exceptionMessage = GetExceptionString(e);
+            string logMessageDate = DateTime.Now.ToLongDateString();
+            string logMessage = logMessageDate + " " + message + " " + exceptionMessage +  Environment.NewLine;
+
+            return logMessage;
+        }
+
+        private static string GetExceptionString(Exception error)
+        {
+            if(error == null)
+            {
+                return "";
+            }
+            string errorString = "EXCEPTION: ";
+            Exception currentError = error;
+            while (currentError != null)
+            {
+                errorString += currentError.Message + " ";
+                currentError = currentError.InnerException;
+            }
+
+            return errorString;
+        }
     }
 }
